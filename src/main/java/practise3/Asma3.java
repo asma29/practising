12452cfa8133b1feddbbb400711dc7470384215c
asma <asmabegum.md@gmail.com>
2018-04-03 16:18:33 +0530

    this is practise3 program

---
+++ src/main/java/practise3/Asma3.java
@@ -0,0 +1,10 @@
+package practise3;
+
+public class Asma3 {
+	
+	public String sample () {
+		return" this is practise3 ";
+		
+	}
+
+}
---
+++ src/test/java/practise3/Asma3.java
@@ -0,0 +1,14 @@
+package practise3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Asma3 {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
